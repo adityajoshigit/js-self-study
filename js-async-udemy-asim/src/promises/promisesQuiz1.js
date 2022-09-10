65552daf367b5dab8f820d8cd41e5ac8b9706a05
Aditya Joshi <adityasjoshi96@gmail.com>
2022-09-10 19:39:58 -0400

    promises quiz 1 done

---
+++ js-async-udemy-asim/src/promises/promisesQuiz1.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+function readFile(filename, encoding) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, encoding, (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+}
+
+readFile("../callbacks/callbackQuiz1.js", "utf-8")
+    .then(
+        res => console.log("File\'s data is ===>\n\n" + res),
+        error => console.log("Encountered some error ===> \n\n " + error)
+    );
